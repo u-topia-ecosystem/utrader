00165adcdc5e281a12426207116ec4f797a1815d
Dev-utopia <hamza@u-topia.com>
2024-08-05 16:54:45 +0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,39 +1,15 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "next/document";
-import React from "react";
-import { CustomizationSettingsInterface, generateInitialCSS, getColorConfigFiles } from "../utils/themeUtils";
+import { Html, Head, Main, NextScript } from "next/document";
 
-interface Props {
-    lightColorsConfig: CustomizationSettingsInterface | null;
-    darkColorsConfig: CustomizationSettingsInterface | null;
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <div id="modal_portal"></div>
+        <div id="overlay_portal"></div>
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-class MyDocument extends Document<Props> {
-    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps & Props> {
-        const initialProps = await Document.getInitialProps(ctx);
-
-        const { darkColorsConfig, lightColorsConfig } = getColorConfigFiles();
-
-        return { ...initialProps, lightColorsConfig, darkColorsConfig };
-    }
-
-    render() {
-        const { lightColorsConfig, darkColorsConfig } = this.props;
-        const { htmlStyles, bodyStyles } = generateInitialCSS(lightColorsConfig, darkColorsConfig);
-
-        return (
-            // @ts-ignore
-            <Html style={{ cssText: htmlStyles }}>
-                <Head></Head>
-                {/* @ts-ignore */}
-                <body style={{ cssText: bodyStyles }}>
-                    <Main />
-                    <div id="modal_portal"></div>
-                    <div id="overlay_portal"></div>
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
-}
-
-export default MyDocument;
